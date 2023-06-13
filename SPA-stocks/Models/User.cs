165e78fbb422419cdca660891b42c58e689e2a57
HubertSienicki <businesssienicki@gmail.com>
2023-06-13 20:15:35 +0200

    Create user model

---
+++ SPA-stocks/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace SPA_stocks.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public DateTime CreatedAt { get; set; }
+}

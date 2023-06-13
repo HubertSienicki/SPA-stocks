998c5642cc22428f856e714977b11480eabf2796
HubertSienicki <businesssienicki@gmail.com>
2023-06-13 21:16:41 +0200

    Add features for user model

--- SPA-stocks/Models/User.cs
+++ SPA-stocks/Models/User.cs
@@ -1,9 +1,22 @@
-﻿namespace SPA_stocks.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SPA_stocks.Models;
 
 public class User
 {
+    [Required]
     public int Id { get; set; }
+
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
     public string Password { get; set; }
-    public DateTime CreatedAt { get; set; }
+
+    [Required]
+    [DataType(DataType.DateTime)]
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
 }
